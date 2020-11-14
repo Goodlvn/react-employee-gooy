3b65cda532a10de300048b5f471bad7483fac9c1
JD Martinez <focus4ursoul@gmail.com>
2020-11-14 12:24:24 -0800

    success: connect with API

--- src/components/DisplayEmployees/index.js
+++ src/components/DisplayEmployees/index.js
@@ -1,6 +1,18 @@
 import React from 'react'
+import API from "../../utils/API"
 
 export default function index() {
+
+    async function grabEmp() {
+
+       const {data: { results: data }} = await API.getEmployeeData();
+
+       console.log(data);
+    }
+
+    grabEmp();
+
+
     return (
         <div>
             TEST: SEE EMPLOYEES HERE
--- src/utils/API.js
+++ src/utils/API.js
@@ -1,24 +1,9 @@
 import axios from "axios";
 
-const queryUrl = "https://randomuser.me/api/?results=25";
+const queryUrl = "https://randomuser.me/api/?results=5";
 
 export default {
     getEmployeeData: () => {
         return axios.get(queryUrl)
     }
 }
-
-
-// import axios from "axios";
-
-// // Export an object containing methods we'll use for accessing the Wikipedia API
-
-// export default {
-//   searchTerms: function(query) {
-//     return axios.get(
-//       "https://en.wikipedia.org/w/api.php?action=opensearch&search=" +
-//         query +
-//         "&limit=1&format=json&origin=*"
-//     );
-//   }
-// };
